079208532ad056482c367ed8277dad5f3baee1a6
Aman Kumar <amank.s1909@gmail.com>
2026-03-26 12:54:03 +0530

    fixed template folder

--- wizardtechproject/settings.py
+++ wizardtechproject/settings.py
@@ -54,9 +54,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'wizardtechproject.urls'
 
 TEMPLATES = [
+    
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
